c272936e43fb170382e6f35c59500398dfb561b7
tanjilahamed99 <tanjil113355@gmail.com>
2026-03-03 13:21:27 +0600

    final commit

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,18 +1,12 @@
-import configuration from "@/config/configuration";
 import IO from "socket.io-client";
 
 let socket = null;
 export const initSocket = (token: string) => {
   if (!socket) {
-    // socket = IO("sewamahe.in", {
-    //     auth: { token },
-    //     transports: ["websocket"],
-    //     withCredentials: true,
-    // });
-    socket = IO(configuration.url, {
-      auth: { token },
-      transports: ["websocket"],
-      withCredentials: true,
+    socket = IO("sewamahe.in", {
+        auth: { token },
+        transports: ["websocket"],
+        withCredentials: true,
     });
   }
   return socket;
